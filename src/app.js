d98106b1d5548fb0f68be412cb772d82e00827c7
m.dynkin <m.dynkin@fbs.com>
2022-11-02 19:18:54 +0200

    1

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log("Server running on port 3000");
